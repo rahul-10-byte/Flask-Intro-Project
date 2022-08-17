9981a283ecb9bc516cba071474d914fa45ce6b58
rahul-10-byte <kalyankarrahul500@gmail.com>
2022-08-18 00:39:48 +0530

    Connected to SQLite DB

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask
+
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+from sqlalchemy import PrimaryKeyConstraint
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# if __name__ == 'main':
-#     app.run(debug=True)
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.title} - {self.desc}"
+
+@app.route("/", methods = ['POST', 'GET'])
+def hello_world():
+    if request.method == 'POST':
+        title = request.form['title']
+        desc  = request.form['desc']
+        # print(title, desc)
+        todo = Todo(title=title, desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+    allTodo = Todo.query.all()
+
+    return render_template('index.html', allTodo = allTodo)
+    
+@app.route("/products")
+def products():
+    return "<p>This is second page</p>"
+if __name__ == '__main__':
+    app.run(debug=True)
